098dfb7b91dd59dab35b24abc308ae597f34e7cf
x4nt <addisonshaw93@gmail.com>
2019-08-25 18:21:28 -0500

    Adding insta link and some dope icons

--- app/components/shared/NavBar.jsx
+++ app/components/shared/NavBar.jsx
@@ -7,17 +7,38 @@ const NavBar = ({ currentPage }) => (
     <ul className="flex justify-end">
       <li className="px-4 py-2 m-2">
         <div className={currentPage === 0 ? 'text-gray-800' : 'text-gray-500 hover:text-gray-800'}>
-          <Link to="/">Portfolio</Link>
+          <Link to="/">
+            <div className="flex flex-row">
+              <div className="flex flex-col justify-center mr-1 pb-1">Portfolio</div>
+              <div className="flex flex-col justify-center">
+                <ion-icon name="color-palette" />
+              </div>
+            </div>
+          </Link>
         </div>
       </li>
       <li className="px-4 py-2 m-2">
         <div className={currentPage === 1 ? 'text-gray-800' : 'text-gray-500 hover:text-gray-800'}>
-          <Link to="/about">About</Link>
+          <Link to="/about">
+            <div className="flex flex-row">
+              <div className="flex flex-col justify-center mr-1 pb-1">About</div>
+              <div className="flex flex-col justify-center">
+                <ion-icon name="information-circle" />
+              </div>
+            </div>
+          </Link>
         </div>
       </li>
       <li className="px-4 py-2 m-2">
         <div className={currentPage === 2 ? 'text-gray-800' : 'text-gray-500 hover:text-gray-800'}>
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact">
+            <div className="flex flex-row">
+              <div className="flex flex-col justify-center mr-1 pb-1">Contact</div>
+              <div className="flex flex-col justify-center">
+                <ion-icon name="mail" />
+              </div>
+            </div>
+          </Link>
         </div>
       </li>
     </ul>
