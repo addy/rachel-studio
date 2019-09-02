43b83ebb2f28ecdee83d1626469aa3c9976a8061
x4nt <addisonshaw93@gmail.com>
2019-09-02 16:34:49 -0500

    And some more

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -7,28 +7,28 @@ const About = () => (
   <Fragment>
     <NavBar currentPage={1} />
     <div className="container mx-auto">
-      <div className="flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row">
-        <div className="w-full sm:w-full md:w-2/6 lg:w-2/6 xl:w-2/6 flex flex-col justify-center p-20 mx-auto md:mx-0 lg:mx-0 xl:mx-0">
+      <div className="flex flex-col lg:flex-row xl:flex-row">
+        <div className="w-1/2 lg:w-2/6 xl:w-2/6 flex flex-col justify-center p-4 lg:p-20 xl:p-20 mx-auto lg:mx-0 xl:mx-0">
           <img
             src={Rachel}
             alt="Rachel"
             className="rounded-full flex items-center justify-center overflow-hidden shadow-2xl"
           />
         </div>
-        <div className="w-full sm:w-full md:w-4/6 lg:w-4/6 xl:w-4/6 p-20 md:h-full lg:h-full xl:h-full mx-auto md:mx-0 lg:mx-0 xl:mx-0">
+        <div className="w-full lg:w-4/6 xl:w-4/6 p-4 lg:p-20 xl:p-20 lg:h-full xl:h-full mx-auto lg:mx-0 xl:mx-0">
           <div className="flex items-center mx-auto">
             <div>
-              <p className="mb-2">
+              <p className="mb-4">
                 Rachel Shaw is an oil painter based out of Kansas City. She graduated with a BFA
                 from Kansas State University with a focus in painting.
               </p>
-              <p className="mb-2">
+              <p className="mb-4">
                 While her primary passion is portraiture, she has recently begun experimenting with
                 small still lives and abstract compositions. Due to the personalized nature of her
                 style, most of her paintings are done on a commission basis. There is nothing Rachel
                 likes better than representing loved ones and objects through paint.
               </p>
-              <p className="mb-2">
+              <p className="mb-4">
                 Please do not hesitate to reach out to her with your painting proposals. She is a
                 motivated artist who genuinely wants to work with you!
               </p>
