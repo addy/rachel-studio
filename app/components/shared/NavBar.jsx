f4d54ce3376f00f0574b494fe34eae8bdb72a886
x4nt <addisonshaw93@gmail.com>
2019-09-02 18:14:37 -0500

    Try adding extra space

--- app/components/shared/NavBar.jsx
+++ app/components/shared/NavBar.jsx
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom';
 
 const NavBar = ({ currentPage, noMargin }) => (
   <div className={`container mx-auto${noMargin ? '' : ' lg:mb-10 xl:mb-10'}`}>
-    <ul className="flex justify-center lg:justify-end xl:justify-end lg:pr-4 xl:pr-4 mx-auto lg:mx-0 xl:mx-0">
-      <li className="py-2 m-2">
+    <div className="flex justify-center lg:justify-end xl:justify-end lg:pr-4 xl:pr-4 mx-auto lg:mx-0 xl:mx-0">
+      <div className="w-1/3 lg:w-auto xl:w-auto text-center py-2 m-2">
         <div className={currentPage === 0 ? 'text-gray-800' : 'text-gray-500 hover:text-gray-800'}>
           <Link to="/portfolio">Portfolio</Link>
         </div>
-      </li>
-      <li className="py-2 m-2">
+      </div>
+      <div className="w-1/3 lg:w-auto xl:w-auto text-center py-2 m-2">
         <div className={currentPage === 1 ? 'text-gray-800' : 'text-gray-500 hover:text-gray-800'}>
           <Link to="/about">About</Link>
         </div>
-      </li>
-      <li className="py-2 m-2">
+      </div>
+      <div className="w-1/3 lg:w-auto xl:w-auto text-center py-2 m-2">
         <div className={currentPage === 2 ? 'text-gray-800' : 'text-gray-500 hover:text-gray-800'}>
           <Link to="/contact">Contact</Link>
         </div>
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
 );
 
