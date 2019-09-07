d09bbd17a017425673159ea4da4bd9ad380bb007
x4nt <addisonshaw93@gmail.com>
2019-09-07 11:43:27 -0500

    Trying to fix google analytics

---
+++ app/components/shared/GAListener.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+import ReactGA from 'react-ga';
+
+const sendPageView = location => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+};
+
+const GAListener = ({ children, trackingID, history }) => {
+  useEffect(() => {
+    ReactGA.initialize(trackingID);
+    sendPageView(history.location);
+    return history.listen(sendPageView);
+  }, []);
+
+  return children;
+};
+
+GAListener.propTypes = {
+  children: PropTypes.node,
+  trackingID: PropTypes.string,
+  history: PropTypes.shape({
+    listen: PropTypes.func
+  })
+};
+
+export default withRouter(GAListener);
--- app/routers/Router.jsx
+++ app/routers/Router.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment, Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import GAListener from '../components/shared/GAListener';
 import Loading from '../components/shared/Loading';
 import Index from '../components/Index';
 
@@ -11,14 +11,16 @@ const Contact = lazy(() => import('../components/Contact'));
 const Router = () => (
   <Fragment>
     <BrowserRouter>
-      <Suspense fallback={Loading}>
-        <Switch>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-        </Switch>
-      </Suspense>
+      <GAListener trackingID="UA-136081513-3">
+        <Suspense fallback={Loading}>
+          <Switch>
+            <Route exact path="/" component={Index} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
+        </Suspense>
+      </GAListener>
     </BrowserRouter>
   </Fragment>
 );
