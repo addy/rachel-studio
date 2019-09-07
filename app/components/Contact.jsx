e0fb4f4e9ea7561f705f09d1650939e098ec1fea
x4nt <addisonshaw93@gmail.com>
2019-09-07 10:47:38 -0500

    Making Contact page better for mobile

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -106,7 +106,7 @@ class Contact extends Component {
 
     return (
       // tailwindcss: bg-green-100 bg-red-100 border border-green-400 border-red-400 text-green-700 text-red-700 text-green-500 text-red-500 px-4 py-3 mb-5
-      <div className="flex flex-col items-center justify-center w-full h-full">
+      <div className="flex flex-col items-center justify-center w-full h-full px-10 lg:px-0">
         {formResponseCode && (
           <div
             className={`w-1/2 bg-${color}-100 border border-${color}-400 text-${color}-700 px-4 py-3 rounded relative mx-auto mb-5`}
@@ -135,9 +135,9 @@ class Contact extends Component {
             </button>
           </div>
         )}
-        <form className="w-1/2">
+        <form className="w-full lg:w-1/2">
           <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <div className="w-full lg:w-1/2 px-3 mb-6 lg:mb-0">
               <label
                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 htmlFor="grid-first-name"
@@ -154,7 +154,7 @@ class Contact extends Component {
                 />
               </label>
             </div>
-            <div className="w-full md:w-1/2 px-3">
+            <div className="w-full lg:w-1/2 px-3">
               <label
                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 htmlFor="grid-last-name"
