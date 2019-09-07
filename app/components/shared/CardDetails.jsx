fbc16aa2c56b70637fd9d125ae5cdec7f61b02c2
x4nt <addisonshaw93@gmail.com>
2019-09-07 10:51:02 -0500

    And the Portfolio page

--- app/components/shared/CardDetails.jsx
+++ app/components/shared/CardDetails.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const CardDetails = ({ src, alt, title, text }) => (
   <div className="w-full lg:w-1/3 xl:w-1/3 text-center px-4 py-2 m-2s mx-auto">
-    <div className="max-w-lg rounded overflow-hidden shadow-lg mx-auto">
+    <div className="w-full rounded overflow-hidden shadow-lg">
       <img className="lazy w-full" src={src} alt={alt} />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{title}</div>
