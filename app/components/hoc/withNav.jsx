8f27a31b8017c7ccd18e5c0ecdc9f6aa7a365493
x4nt <addisonshaw93@gmail.com>
2019-09-08 17:52:27 -0500

    React Router complaining about not being able to handle functions..

--- app/components/hoc/withNav.jsx
+++ app/components/hoc/withNav.jsx
@@ -2,12 +2,15 @@ import React, { Fragment } from 'react';
 import NavBar from '../shared/NavBar';
 
 const withNav = (WrappedComponent, canLogin, toggleModal, index = undefined) => {
-  return props => (
+  const wrapped = props => (
     <Fragment>
       <NavBar currentPage={index} canLogin={canLogin} toggleModal={toggleModal} />
       <WrappedComponent {...props} />
     </Fragment>
   );
+
+  const Wrapped = wrapped;
+  return Wrapped;
 };
 
 export default withNav;
--- app/routers/Router.jsx
+++ app/routers/Router.jsx
@@ -2,13 +2,13 @@ import React, { Fragment, Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import GAListener from '../components/shared/GAListener';
 import Loading from '../components/shared/Loading';
-import Index from '../components/Index';
+import index from '../components/Index';
 import loginHooks from '../components/hooks/loginHooks';
 import withNav from '../components/hoc/withNav';
 
-const Portfolio = lazy(() => import('../components/Portfolio'));
-const About = lazy(() => import('../components/About'));
-const Contact = lazy(() => import('../components/Contact'));
+const portfolio = lazy(() => import('../components/Portfolio'));
+const about = lazy(() => import('../components/About'));
+const contact = lazy(() => import('../components/Contact'));
 
 const Router = () => {
   const { values, handleLoginModalToggle, component } = loginHooks({
@@ -17,29 +17,21 @@ const Router = () => {
     token: undefined
   });
 
+  const Index = withNav(index, values.canLogin, handleLoginModalToggle, 0);
+  const Portfolio = withNav(portfolio, values.canLogin, handleLoginModalToggle, 1);
+  const About = withNav(about, values.canLogin, handleLoginModalToggle, 2);
+  const Contact = withNav(contact, values.canLogin, handleLoginModalToggle, 3);
+
   return (
     <Fragment>
       <BrowserRouter>
         <GAListener trackingID="UA-136081513-3">
           <Suspense fallback={Loading}>
             <Switch>
-              <Route
-                exact
-                path="/"
-                component={withNav(Index, values.canLogin, handleLoginModalToggle, 0)}
-              />
-              <Route
-                path="/portfolio"
-                component={withNav(Portfolio, values.canLogin, handleLoginModalToggle, 1)}
-              />
-              <Route
-                path="/about"
-                component={withNav(About, values.canLogin, handleLoginModalToggle, 2)}
-              />
-              <Route
-                path="/contact"
-                component={withNav(Contact, values.canLogin, handleLoginModalToggle, 3)}
-              />
+              <Route exact path="/" component={Index} />
+              <Route path="/portfolio" component={Portfolio} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
             </Switch>
           </Suspense>
         </GAListener>
