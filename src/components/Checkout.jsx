f3adcc56db6fa6a877dee3e033730c6d35cab030
x4nt <addisonshaw93@gmail.com>
2019-09-15 03:05:07 -0500

    Can now memoize two more main pages

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import CheckoutForm from './shared/CheckoutForm';
 
-const Checkout = () => (
-  <StripeProvider apiKey="pk_test_r7NDcsBCH8rsKv9bZzGpTEgx00WOJuql7Y">
-    <div className="container mx-auto h-full">
-      <Elements>
-        <CheckoutForm />
-      </Elements>
-    </div>
-  </StripeProvider>
+const Checkout = memo(
+  () => (
+    <StripeProvider apiKey="pk_test_r7NDcsBCH8rsKv9bZzGpTEgx00WOJuql7Y">
+      <div className="container mx-auto h-full">
+        <Elements>
+          <CheckoutForm />
+        </Elements>
+      </div>
+    </StripeProvider>
+  ),
+  () => true
 );
 
 export default Checkout;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,46 +1,49 @@
-import React from 'react';
+import React, { memo } from 'react';
 import Form from './shared/Form';
 import { useStateValue } from './hooks/State';
 import { flatten } from './shared/utils';
 
-const Contact = () => {
-  const [, dispatch] = useStateValue();
+const Contact = memo(
+  () => {
+    const [, dispatch] = useStateValue();
 
-  const onSubmit = async values => {
-    const { firstName, lastName, email, message } = flatten(Object.values(values));
+    const onSubmit = async values => {
+      const { firstName, lastName, email, message } = flatten(Object.values(values));
 
-    const res = await fetch('/api/contact', {
-      method: 'POST',
-      mode: 'same-origin',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        firstName,
-        lastName,
-        email,
-        message
-      })
-    });
+      const res = await fetch('/api/contact', {
+        method: 'POST',
+        mode: 'same-origin',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          email,
+          message
+        })
+      });
 
-    if (res.ok) dispatch({ type: 'contactSuccess' });
-    else dispatch({ type: 'contactFailure' });
-  };
+      if (res.ok) dispatch({ type: 'contactSuccess' });
+      else dispatch({ type: 'contactFailure' });
+    };
 
-  return (
-    <div className="flex flex-col items-center justify-center w-full h-full px-10 lg:px-0">
-      <Form
-        fields={[
-          { name: 'first name', type: 'input', placeholder: 'Jane' },
-          { name: 'last name', type: 'input', placeholder: 'Doe' },
-          { name: 'email', type: 'email', placeholder: 'jane.doe@email.com' },
-          { name: 'message', type: 'text', placeholder: '...' }
-        ]}
-        submit={onSubmit}
-      />
-    </div>
-  );
-};
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-full px-10 lg:px-0">
+        <Form
+          fields={[
+            { name: 'first name', type: 'input', placeholder: 'Jane' },
+            { name: 'last name', type: 'input', placeholder: 'Doe' },
+            { name: 'email', type: 'email', placeholder: 'jane.doe@email.com' },
+            { name: 'message', type: 'text', placeholder: '...' }
+          ]}
+          submit={onSubmit}
+        />
+      </div>
+    );
+  },
+  () => true
+);
 
 export default Contact;
